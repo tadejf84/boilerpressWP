379222b7ba3172800db698f7f94bfbc61e37a4be
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-20 13:17:11 +0200

    default page template

--- page.php
+++ page.php
@@ -8,15 +8,16 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<div class="container">
-    <main class="main main--pages">
+<main class="main main--pages">
+    <div class="container">
 
         <?php if(have_posts()) : ?>
             <?php while(have_posts()) : the_post(); ?>
                 <?php get_template_part( 'template-parts/loop/content-page' ); ?>
             <?php endwhile; ?>
         <?php endif; ?>
+        
+    </div>
+</main>
 
-    </main>
-</div>
     
