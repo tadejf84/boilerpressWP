650f5112fc064a34dfa9782c40e99987d30b4826
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-20 13:38:12 +0200

    header template

--- header.php
+++ header.php
@@ -10,14 +10,26 @@ defined( 'ABSPATH' ) || exit;
 
 <header class="site-header">
 
-    <div class="container">
+    <div class="container d-flex justify-content-between align-items-center">
+
+        <div class="site-header__brand">
+            <?php if ( ! has_custom_logo() ) : ?>
+                <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+            <?php else : ?>
+                <?php the_custom_logo(); ?>
+            <?php endif; ?>
+        </div>
+
         <?php wp_nav_menu(
             array(
-                'theme_location'  => 'main-menu',
-                'menu_class'      => 'site-header__navigation',
-                'menu_id'         => 'main-menu'
+                'theme_location'    => 'main-menu',
+                'container'         => 'nav',
+                'container_class'   => 'site-header__navigation',
+                'menu_class'        => 'main-menu d-flex list-unstyled mb-0',
+                'menu_id'           => 'main-menu',
             )
         ); ?>
+
     </div>
 
 </header>
