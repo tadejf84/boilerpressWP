2e7295e6ed2a09fe3565620fbf857e88f6470ae6
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-20 11:26:53 +0200

    helper functions

--- inc/helpers.php
+++ inc/helpers.php
@@ -2,6 +2,10 @@
 namespace BoilerPress\Helpers;
 
 /**
- * All helpers
+ * Your theme helper functions go here
  * 
+ * @package BoilerPressWP
  */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
