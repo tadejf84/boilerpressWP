a77dc190461a02eece64ad38eddd25d3e727e382
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-20 13:17:59 +0200

    front page template

--- front-page.php
+++ front-page.php
@@ -8,14 +8,14 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<div class="container">
-    <main class="main main--home">
-        
+<main class="main main--home">
+    <div class="container">
+    
         <?php if(have_posts()) : ?>
             <?php while(have_posts()) : the_post(); ?>
                 <?php get_template_part( 'template-parts/loop/content-page' ); ?>
             <?php endwhile; ?>
         <?php endif; ?>
-
-    </main>
-</div>
+        
+    </div>
+</main>
