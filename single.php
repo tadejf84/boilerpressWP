b8ec53865da05ceddec135069701f679e40d4ba8
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-20 14:13:22 +0200

    single post template

--- single.php
+++ single.php
@@ -12,10 +12,19 @@ defined( 'ABSPATH' ) || exit;
     <main class="main main--single">
 
         <?php if( have_posts() ) : ?>
+            
             <?php while( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'template-parts/loop/content-single' ); ?>
             <?php endwhile; ?>
-            <?php BoilerPress\Nav\post_navigation(); ?>
+
+            <?php BoilerPress\Nav\post_navigation(); // post navigation ?>
+
+            <?php
+            // If comments are open and there is at least 1 comment, show comment templates
+            if ( comments_open() || get_comments_number() ) {
+                comments_template();
+            }
+            ?>
         <?php endif; ?>
 
     </main>
