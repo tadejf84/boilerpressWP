ea7f9f2d3304d4d3bd9941afade79ded15783ede
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-20 11:23:47 +0200

    theme functions

--- functions.php
+++ functions.php
@@ -5,8 +5,8 @@ defined( 'ABSPATH' ) || exit;
  * require includes
  * 
  */
-require 'inc/helpers.php';
 require 'inc/globals.php';
+require 'inc/helpers.php';
 require 'inc/setup.php';
 require 'inc/assets.php';
 require 'inc/wrapper.php';
