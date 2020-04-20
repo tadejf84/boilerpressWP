6a77d350931913eec31fd0ac08ab6d180419b926
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-20 13:59:35 +0200

    search form template

--- searchform.php
+++ searchform.php
@@ -1,15 +1,17 @@
-boilerpress
+<?php
+/**
+ * Search form template
+ * 
+ * @package BoilerPressWP
+ */
+
 defined( 'ABSPATH' ) || exit;
 ?>
 
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 	<label class="sr-only" for="s"><?php esc_html_e( 'Search', 'boilerpress' ); ?></label>
-	<div class="input-group">
-		<input class="field form-control" id="s" name="s" type="text"
-			placeholder="<?php esc_attr_e( 'Search &hellip;', 'boilerpress' ); ?>" value="<?php the_search_query(); ?>">
-		<span class="input-group-append">
-			<input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit"
-			value="<?php esc_attr_e( 'Search', 'boilerpress' ); ?>">
-		</span>
+	<div class="input-group d-flex">
+		<input class="field form-control" id="s" name="s" type="text" placeholder="<?php esc_attr_e( 'Search &hellip;', 'boilerpress' ); ?>" value="<?php the_search_query(); ?>">
+		<input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit" value="<?php esc_attr_e( 'Search', 'boilerpress' ); ?>">
 	</div>
 </form>
