ad60dfe964eeacc6a75e6c45cd117349316cfe23
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-20 12:32:23 +0200

    customize the theme

--- inc/customize.php
+++ inc/customize.php
@@ -18,12 +18,14 @@ defined( 'ABSPATH' ) || exit;
  * @return string
  */
 if ( ! function_exists( 'custom_excerpt_more_text' ) ) {
+
 	function custom_excerpt_more_text( $more ) {
 		if ( ! is_admin() ) {
 			$more = '';
 		}
 		return $more;
-	}
+    }
+    
 }
 
 add_filter( 'excerpt_more', __NAMESPACE__ . '\\custom_excerpt_more_text' );
@@ -37,12 +39,14 @@ add_filter( 'excerpt_more', __NAMESPACE__ . '\\custom_excerpt_more_text' );
  * @return string
  */
 if ( ! function_exists( 'customize_excerpt_more_link' ) ) {
+
 	function customize_excerpt_more_link( $post_excerpt ) {
 		if ( ! is_admin() ) {
 			$post_excerpt = $post_excerpt . '<a class="btn btn--read-more" href="' . esc_url( get_permalink( get_the_ID() ) ) . '">' . __( 'Read More', 'boilerpress' ) . '</a>';
 		}
 		return $post_excerpt;
-	}
+    }
+    
 }
 
 add_filter( 'wp_trim_excerpt',  __NAMESPACE__ . '\\customize_excerpt_more_link' );
@@ -55,21 +59,25 @@ add_filter( 'wp_trim_excerpt',  __NAMESPACE__ . '\\customize_excerpt_more_link' 
  *
  * @return string
  */
-function customize_archive_title($title) {
-	if ( is_category() ) {
-        $title = single_cat_title( '', false );
-    } elseif ( is_tag() ) {
-        $title = single_tag_title( '', false );
-    } elseif ( is_author() ) {
-        $title = get_the_author() . ' ' . _e( 'Archives', 'boilderpress' );
-    } elseif ( is_post_type_archive() ) {
-        $title = post_type_archive_title( '', false );
-    } elseif ( is_tax() ) {
-        $title = single_term_title( '', false );
-    } else {
-        $title = _e( 'Blog', 'boilderpress' );
+if ( ! function_exists( 'customize_archive_title' ) ) {
+
+    function customize_archive_title( $title ) {
+        if ( is_category() ) {
+            $title = single_cat_title( '', false );
+        } elseif ( is_tag() ) {
+            $title = single_tag_title( '', false );
+        } elseif ( is_author() ) {
+            $title = get_the_author() . ' ' . _e( 'Archives', 'boilderpress' );
+        } elseif ( is_post_type_archive() ) {
+            $title = post_type_archive_title( '', false );
+        } elseif ( is_tax() ) {
+            $title = single_term_title( '', false );
+        } else {
+            $title = _e( 'Blog', 'boilderpress' );
+        }
+        return $title;
     }
-    return $title;
+
 }
 
 add_filter( 'get_the_archive_title', __NAMESPACE__ . '\\customize_archive_title' );
