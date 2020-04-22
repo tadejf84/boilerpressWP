098decadccc47ebae7afc07786f7ba4908b85fe3
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 11:02:12 +0200

    theme constants

--- inc/globals.php
+++ inc/globals.php
@@ -10,10 +10,26 @@ defined( 'ABSPATH' ) || exit;
 
 
 /**
- * Define stylesheets and scripts dir and URI
+ * Define theme directory and URI
  * 
  */
-define( "BOILERPRESS_STYLESHEETS_DIR", get_template_directory() . '/dist/css/' );
-define( "BOILERPRESS_STYLESHEETS_URI", get_template_directory_uri() . '/dist/css/' );
-define( "BOILERPRESS_SCRIPTS_DIR", get_template_directory() . '/dist/js/' );
-define( "BOILERPRESS_SCRIPTS_URI", get_template_directory_uri() . '/dist/js/' );
+define( "BOILERPRESS_THEME_DIR", get_template_directory() );
+define( "BOILERPRESS_THEME_URI", get_template_directory_uri() );
+
+
+/**
+ * Define stylesheets and scripts dir and URI
+ * 
+ * IMPORTANT! They are used in inc/assets.php
+ */
+define( "BOILERPRESS_STYLESHEETS_DIR", BOILERPRESS_THEME_DIR . '/dist/css/' );
+define( "BOILERPRESS_STYLESHEETS_URI", BOILERPRESS_THEME_URI . '/dist/css/' );
+define( "BOILERPRESS_SCRIPTS_DIR", BOILERPRESS_THEME_DIR . '/dist/js/' );
+define( "BOILERPRESS_SCRIPTS_URI", BOILERPRESS_THEME_URI . '/dist/js/' );
+
+
+/**
+ * Define media URI - for images & videos
+ * 
+ */
+define( "BOILERPRESS_MEDIA_URI", BOILERPRESS_THEME_URI . '/dist/img/' );
