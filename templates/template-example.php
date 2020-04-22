729cb4d6ef9eed3bfd03cf276b43d2ada15c033c
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 12:03:42 +0200

    custom template example

--- templates/template-example.php
+++ templates/template-example.php
@@ -10,14 +10,14 @@
 
 <main class="main">
     <div class="container">
-        
+
         <?php if(have_posts()) : ?>
             <?php while(have_posts()) : the_post(); ?>
                 <?php get_template_part( 'template-parts/loop/content-page' ); ?>
             <?php endwhile; ?>
         <?php endif; ?>
 
-	</div>
+    </div>
 </main>
 
 
