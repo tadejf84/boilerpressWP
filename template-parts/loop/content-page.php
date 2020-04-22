645c798d509e2d0a86aaab603980786dc5a361e2
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 12:00:36 +0200

    template parts

--- template-parts/loop/content-page.php
+++ template-parts/loop/content-page.php
@@ -10,13 +10,13 @@ defined( 'ABSPATH' ) || exit;
 
 <article <?php post_class('article-page'); ?> id="post-<?php the_ID(); ?>">
 
-	<header class="article-page__header">
+    <header class="article-page__header">
         <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
         <h1 class="article-page__title"><?php the_title(); ?></h1>
-	</header>
+    </header>
 
-	<div class="article-page__content">
-		<?php the_content(); ?>
+    <div class="article-page__content">
+        <?php the_content(); ?>
     </div>
 
 </article>
