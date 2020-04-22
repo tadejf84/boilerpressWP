8228bbef81ef1384ca643b179ccba6906fb5047c
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 13:25:38 +0200

    main script file

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,19 @@
+// import SASS files to webpack
 import '../sass/main.scss';
 
-let test = '3343434'
-console.log('scripts loaded!!!');
+// if using jQuery, otherwise remove this part and write vanilla JS instead
+(function ($, root, undefined) {
+
+	$(function () {
+
+		'use strict';
+
+		$(document).ready(function() {
+
+            console.log('main script loaded!!!');
+
+		});
+
+	});
+
+})(jQuery, this);
