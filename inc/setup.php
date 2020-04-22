06c64e872ec345c48f451a58ec5d702e0e4e5abe
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 11:43:29 +0200

    theme basic setup

--- inc/setup.php
+++ inc/setup.php
@@ -12,48 +12,48 @@ defined( 'ABSPATH' ) || exit;
 
 if ( ! function_exists( 'theme_setup' ) ) {
 
-	function theme_setup() {
+    function theme_setup() {
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support( 'automatic-feed-links' );
 
-		// WP Document title
-		add_theme_support( 'title-tag' );
+        // WP Document title
+        add_theme_support( 'title-tag' );
 
-		// WP Nav Menus
-		register_nav_menus( array(
-			'main-menu' => __( 'Main Menu', 'boilerpress' ),
-		) );
+        // WP Nav Menus
+        register_nav_menus( array(
+            'main-menu' => __( 'Main Menu', 'boilerpress' ),
+        ) );
 
-		// This feature allows the use of HTML5 markup for the search forms, comment forms, comment lists, gallery, and caption.
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
+        // This feature allows the use of HTML5 markup for the search forms, comment forms, comment lists, gallery, and caption.
+        add_theme_support( 'html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ) );
 
-		// Post thumbnails support
-		add_theme_support( 'post-thumbnails' );
+        // Post thumbnails support
+        add_theme_support( 'post-thumbnails' );
 
-		// Post formats support
-		add_theme_support( 'post-formats', array(
-			'aside',
-			'image',
-			'video',
-			'quote',
-			'link',
-		) );
+        // Post formats support
+        add_theme_support( 'post-formats', array(
+            'aside',
+            'image',
+            'video',
+            'quote',
+            'link',
+        ) );
 
-		// Custom logo support
-		add_theme_support( 'custom-logo' );
+        // Custom logo support
+        add_theme_support( 'custom-logo' );
 
-		// Responsive embeds support
-		add_theme_support( 'responsive-embeds' );
+        // Responsive embeds support
+        add_theme_support( 'responsive-embeds' );
 
-	}
-	
+    }
+
 }
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\\theme_setup' );
