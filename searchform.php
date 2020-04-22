e859ee309887402ce7b9dbb03d0f7f8900175fa8
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 12:55:23 +0200

    custom search form

--- searchform.php
+++ searchform.php
@@ -13,6 +13,6 @@ defined( 'ABSPATH' ) || exit;
     <label class="sr-only" for="s"><?php esc_html_e( 'Search', 'boilerpress' ); ?></label>
     <div class="input-group d-flex">
         <input class="field form-control" id="s" name="s" type="text" placeholder="<?php esc_attr_e( 'Search &hellip;', 'boilerpress' ); ?>" value="<?php the_search_query(); ?>">
-        <input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit" value="<?php esc_attr_e( 'Search', 'boilerpress' ); ?>">
+        <input class="submit btn btn--primary" id="searchsubmit" name="submit" type="submit" value="<?php esc_attr_e( 'Search', 'boilerpress' ); ?>">
     </div>
 </form>
