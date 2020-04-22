b9fc3d631623d8e9ec29f5a66f5a4dc240f99597
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 12:20:07 +0200

    single post template

--- single.php
+++ single.php
@@ -5,6 +5,7 @@
  * @package BoilerPressWP
  */
 
+// Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
 
@@ -12,12 +13,11 @@ defined( 'ABSPATH' ) || exit;
     <main class="main main--single">
 
         <?php if( have_posts() ) : ?>
-
             <?php while( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'template-parts/loop/content-single' ); ?>
             <?php endwhile; ?>
 
-            <?php BoilerPress\Nav\post_navigation(); // post navigation ?>
+            <?php BoilerPress\Nav\post_navigation(); // post navigation component ?>
 
             <?php
             // If comments are open and there is at least 1 comment, show comment templates
