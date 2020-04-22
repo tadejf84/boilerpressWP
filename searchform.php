07e8c7595fcc900a93b0e524419347b739d89665
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 12:18:39 +0200

    search form template

--- searchform.php
+++ searchform.php
@@ -5,13 +5,14 @@
  * @package BoilerPressWP
  */
 
+// Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
 
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<label class="sr-only" for="s"><?php esc_html_e( 'Search', 'boilerpress' ); ?></label>
-	<div class="input-group d-flex">
-		<input class="field form-control" id="s" name="s" type="text" placeholder="<?php esc_attr_e( 'Search &hellip;', 'boilerpress' ); ?>" value="<?php the_search_query(); ?>">
-		<input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit" value="<?php esc_attr_e( 'Search', 'boilerpress' ); ?>">
-	</div>
+    <label class="sr-only" for="s"><?php esc_html_e( 'Search', 'boilerpress' ); ?></label>
+    <div class="input-group d-flex">
+        <input class="field form-control" id="s" name="s" type="text" placeholder="<?php esc_attr_e( 'Search &hellip;', 'boilerpress' ); ?>" value="<?php the_search_query(); ?>">
+        <input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit" value="<?php esc_attr_e( 'Search', 'boilerpress' ); ?>">
+    </div>
 </form>
