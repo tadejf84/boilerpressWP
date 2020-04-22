d74dce619660dcd5944bc19311340320acd094df
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 12:05:01 +0200

    404 template

--- 404.php
+++ 404.php
@@ -9,14 +9,12 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <main class="main main--404">
+    <div class="container">
 
-	<div class="container">
+        <section class="error-404">
+            <h1 class="page-title"><?php esc_html_e( '404', 'boilerpress' ); ?></h1>
+            <p><?php esc_html_e( 'Nothing found.', 'boilerpress' ); ?></p>
+        </section>
 
-		<section class="error-404">
-			<h1 class="page-title"><?php esc_html_e( '404', 'boilerpress' ); ?></h1>
-			<p><?php esc_html_e( 'Nothing found.', 'boilerpress' ); ?></p>
-		</section>
-
-	</div>
-
+    </div>
 </main>
