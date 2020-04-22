f5a6782f72a7960b040b39a8f2ca78d501644a00
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 11:46:41 +0200

    init widgets

--- inc/widgets.php
+++ inc/widgets.php
@@ -16,28 +16,28 @@ defined( 'ABSPATH' ) || exit;
  * Init widgets
  * 
  */
-//add_action( 'widgets_init', __NAMESPACE__ . '\\widgets_init' );
-
 if ( ! function_exists( 'widgets_init' ) ) {
 
 	function widgets_init() {
-
 		/**
 		 * Example widget
 		 * 
 		 * Change details to your own
 		 */
-		register_sidebar(
-			array(
-				'name'          => __( 'Example Widget', 'boilerpress' ),
-				'id'            => 'example-widget',
-				'description'   => __( 'Your widget description goes here', 'boilerpress' ),
-				'before_widget' => '<div id="%1$s" class="widget %2$s">',
-				'after_widget'  => '</div>',
-				'before_title'  => '<h3 class="widget__title">',
-				'after_title'   => '</h3>',
-			)
-		);
-	}
+        register_sidebar(
+            array(
+                'name'          => __( 'Example Widget', 'boilerpress' ),
+                'id'            => 'example-widget',
+                'description'   => __( 'Your widget description goes here', 'boilerpress' ),
+                'before_widget' => '<div id="%1$s" class="widget %2$s">',
+                'after_widget'  => '</div>',
+                'before_title'  => '<h3 class="widget__title">',
+                'after_title'   => '</h3>',
+            )
+        );
+    }
+    
 } 
+
+//add_action( 'widgets_init', __NAMESPACE__ . '\\widgets_init' );
 
