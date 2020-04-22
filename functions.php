f70c174851d71835bd65d5baa468fd9f1e58b27f
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-04-22 12:36:31 +0200

    functions includes

--- functions.php
+++ functions.php
@@ -8,14 +8,24 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-// Required includes
-require 'inc/globals.php';
-require 'inc/helpers.php';
-require 'inc/setup.php';
-require 'inc/customize.php';
-require 'inc/cpt-register.php';
-require 'inc/assets.php';
-require 'inc/wrapper.php';
-require 'inc/navigation.php';
-require 'inc/widgets.php';
-require 'inc/performance.php';
+
+/**
+ * Function includes
+ * 
+ */
+$boilerpress_includes = array(
+	'globals.php',              // Theme constants
+    'helpers.php',              // Place your own helper functions if needed
+    'setup.php',                // Theme basic setup
+    'customize.php',            // Theme customizations
+    'performance.php',          // Performace & security fixes
+    'cpt-register.php',         // Register custom post type & custom taxonomies
+    'assets.php',               // Enqueue theme assets
+    'wrapper.php',              // Create a HTML Wrapper markup to wrap all templates
+    'navigation.php',           // Custom theme navigation functions - pagination, post navigation, etc.
+    'widgets.php',              // Init widgets
+);
+
+foreach ( $boilerpress_includes as $bp_inc ) {
+	require_once get_template_directory() . '/inc/' . $bp_inc;
+}
